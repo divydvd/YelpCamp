362a355b2ad3271e1c9c12cacd1419498a6cee07
IronMan <divydwivedi@gmail.com>
2022-01-10 19:57:30 +0530

    Styles added

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 15;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            price,
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, sit. Quia aliquam voluptatibus voluptatum rerum, tempora minima? Inventore impedit dolorum similique optio distinctio nam, praesentium delectus quisquam fugiat blanditiis nesciunt?'
         });
         await camp.save();
     }
