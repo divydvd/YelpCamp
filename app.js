d76a1fdeacf17e076e733893af5b5b2bddbab2da
IronMan <divydwivedi@gmail.com>
2022-01-29 00:04:54 +0530

    Try deploying

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const Review = require('./models/review');
 const userRoutes= require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrl= 'mongodb://localhost:27017/yelp-camp'; // process.env.DB_URL
+const dbUrl= process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'; // process.env.DB_URL
 
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
@@ -59,14 +59,16 @@ app.use(helmet({contentSecurityPolicy: false}));
 //     console.log('SESSION STORE ERROR', e);
 // });
 
+const secret = process.env.SECRET || 'secret';
+
 const sessionConfig = {
     store: MongoDBStore.create({
         mongoUrl: dbUrl,
-        secret: 'secret',
+        secret,
         touchAfter: 24 * 60 * 60
     }),
     name: 'session',
-    secret: 'secret',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
